156c591602d4ae2a77bda1ce92e2a40526fba7db
ciocan.cosmin <ciocan.cosmin@gmail.com>
2016-10-21 10:25:43 +0300

    Adding changes

--- src/test/java/AngelaClass.java
+++ src/test/java/AngelaClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.internal.FindsByLinkText;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by cosminciocan on 19/10/16.
@@ -14,14 +15,26 @@ import java.util.List;
 public class AngelaClass {
 
     @Test
-    public void newTest(){
+    public void newTest() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("http://evoportal.evozon.com");
         WebElement element = driver.findElement(By.linkText("SEARCH"));
         element.click();
-        WebElement element1 = driver.findElement(By.cssSelector(".search-wrapper [name=_3_keywords]"));
-        element1.sendKeys("abcv");
+        int x = 0;
+
+        while (!driver.findElement(By.cssSelector(".search-wrapper [name=_3_keywords]")).isDisplayed()) {
+            Thread.sleep(200);
+            if ( x >= 5000)
+                break;
+
+            x+=200;
+        }
+
+
+        driver.findElement(By.cssSelector(".search-wrapper [name=_3_keywords]")).sendKeys("abcv");
+        Thread.sleep(3000);
+
         driver.quit();
     }
 }
