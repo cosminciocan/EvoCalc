6380395b9f156930a20486f19d0fd95eb0ed910e
ciocan.cosmin <ciocan.cosmin@gmail.com>
2016-10-26 10:28:43 +0300

    containerList

--- src/test/java/AngelaClass.java
+++ src/test/java/AngelaClass.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.internal.FindsByLinkText;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,7 +20,8 @@ public class AngelaClass {
     WebDriver driver = new FirefoxDriver();
     By searchField = By.cssSelector(".search-wrapper [name=_3_keywords]");
     int defaultTimeoutValue = 5000;
-
+    By searchButton = By.cssSelector(".search-wrapper [type=image]:not(#search)");
+    List<WebElement> listOfResults = driver.findElements(By.cssSelector(".taglib-search-iterator"));
 
     public void waitForElements(By element, int timeoutMilliseconds) throws InterruptedException {
         int x = 0;
@@ -46,7 +49,15 @@ public class AngelaClass {
         WebElement element = driver.findElement(By.linkText("SEARCH"));
         element.click();
         waitForElements(searchField, defaultTimeoutValue);
-        driver.findElement(searchField).sendKeys("abcv");
+        driver.findElement(searchField).sendKeys("a");
+        waitForElements(searchButton,defaultTimeoutValue);
+        driver.findElement(searchButton).click();
+        Thread.sleep(3000);
+        int max = listOfResults.size()-1;
+        int min = 0;
+        Random generator = new Random();
+        //By title = generator.nextInt(max-min
+
         driver.quit();
     }
 }
