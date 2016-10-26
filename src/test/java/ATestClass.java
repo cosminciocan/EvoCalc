a524770a5cc3c6a44d94e4d910eb06c1026b8b2f
Marius Chesovan <marius.chesovan@yahoo.com>
2016-10-26 10:27:52 +0300

    git-ul meu

--- src/test/java/ATestClass.java
+++ src/test/java/ATestClass.java
@@ -1,37 +1,70 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 /**
  * Created by Moka on 10/19/2016.
  */
 @Test
 public class ATestClass extends Thread {
+    WebDriver driver = new FirefoxDriver();
+    By searchField = By.cssSelector(".search-wrapper [name=_3_keywords]");
+    int defaultTimeoutValue = 5000;
+
+    WebElement container = driver.findElement(By.cssSelector("table > tbody"));
+
+    List <WebElement> titles = container.findElements(By.cssSelector(".asset-entry-title > a "));
+//    for (WebElement element : titles)
+    int nr = randomWithRange(1, titles.size());
+   // titles(nr).click().
+
+
+    int randomWithRange(int min, int max)
+    {
+        int range = (max - min) + 1;
+        return (int)(Math.random() * range) + min;
+    }
+
+    public void waitForElements(By element, int timeoutMilliseconds) throws InterruptedException {
+        int x = 0;
+        boolean found = false;
+
+        while (!found) {
+            try {
+                found = driver.findElement(element).isDisplayed();
+
+            } catch (NoSuchElementException e) {
+                Thread.sleep(200);
+                x += 200;
+            }
+            if (x >= timeoutMilliseconds)
+                Assert.fail("Element '" + element.toString() + "' not found");
+
+        }
+
+    }
+
     @Test
     public void openEvoportal() throws InterruptedException {
-        WebDriver driver = new FirefoxDriver();
+//        WebDriver driver = new FirefoxDriver();
         driver.get("http://evoportal.evozon.com");
         driver.findElement(By.cssSelector(".main-menu li:nth-child(8)")).click();
         //waitForElement(driver.findElement(By.cssSelector(".main-menu li:nth-child(8)")).click());
-        WebElement element=driver.findElement(By.cssSelector(".focus)"));
-        waitForElement(element);
+//        WebElement element=driver.findElement(By.cssSelector(".focus)"));
+
+        By element = By.cssSelector(".main-menu li:nth-child(8)");
+        waitForElements(element, defaultTimeoutValue);
         driver.findElement(By.cssSelector("input[type='text']:nth-child(9)")).sendKeys("template");
         driver.findElement(By.cssSelector("input[type='image']:nth-child(11)")).click();
+        titles.get(nr).click();
 
         // waitForElement(driver.findElement(By.cssSelector(".main-menu li:nth-child(8)")).click(););
     }
-
-
-    public void waitForElement(WebElement element) throws InterruptedException {
-        while (!element.isDisplayed()) {
-            //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            Thread.sleep(400);
-        }
-    }
-
-//        }
-//        driver.quit();
 }
 
