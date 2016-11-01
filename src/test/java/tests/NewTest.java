853281c21e1e91e69911ac579449161ce7a843d9
danazhr <daniela.zaharia@evozon.com>
2016-11-01 10:27:22 +0200

    new DanaPage

--- src/test/java/tests/NewTest.java
+++ src/test/java/tests/NewTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.DanaPage;
 import utils.BasePage;
 
 import java.util.List;
@@ -18,32 +19,13 @@ public class NewTest extends BasePage {
 
 
     @Test
-    public void testMethod() throws InterruptedException {
-        By searchInput = By.cssSelector(".search-wrapper input[name*='3_keywords']");
-        driver.get("https://evoportal.evozon.com");
+    public void testMethod() {
+        DanaPage danaPage = new DanaPage();
 
-        driver.manage().window().maximize();
-        WebElement searchLink = driver.findElement(By.linkText("SEARCH"));
-        searchLink.click();
-
-        waitForElement(searchInput, 5000);
-        WebElement search = driver.findElement(searchInput);
-        search.sendKeys("servicii");
-        search.sendKeys(ENTER);
-
-        By searchResults = By.cssSelector("span.asset-entry-title a");
-        waitForElement(searchResults, 5000);
-        List<WebElement> resultsList = driver.findElements(searchResults);
-        System.out.println("Lista are " + resultsList.size() + " elemente");
-
-        WebElement element = resultsList.get(randomGenerator(0, resultsList.size() - 1));
-        String text = element.getText();
-        element.click();
-
-        By bodyContent = By.cssSelector("#main-content");
-        waitForElement(bodyContent, 5000);
-        WebElement bodyOfPage = driver.findElement(bodyContent);
-        Assert.assertTrue(bodyOfPage.getText().contains(text));
+        danaPage.getHomepage();
+        danaPage.clickOnSearchLink();
+        danaPage.searchForSomething("servicii");
+        danaPage.selectRandomElementFromList();
 
     }
 
