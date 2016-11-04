99cfe0f368d2ed3448513a27ea567b06ba721349
danazhr <daniela.zaharia@evozon.com>
2016-11-04 10:29:10 +0200

    Dana changes with select method

--- src/test/java/pages/DanaPage.java
+++ src/test/java/pages/DanaPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import utils.BasePage;
+
 import java.util.List;
 
 import static org.openqa.selenium.Keys.ENTER;
@@ -12,35 +13,35 @@ import static org.openqa.selenium.Keys.ENTER;
 /**
  * Created by evozon on 11/1/2016.
  */
-public class DanaPage extends BasePage{
+public class DanaPage extends BasePage {
     By searchInput = By.cssSelector(".search-wrapper input[name*='3_keywords']");
     By searchLink = By.linkText("SEARCH");
     By searchResults = By.cssSelector("span.asset-entry-title a");
     By bodyContent = By.cssSelector("#main-content");
     By libraryLink = By.linkText("LIBRARY");
-    By selectItemsPerPage = By.cssSelector("#_evozonlibrary_WAR_EvozonLibraryportlet_ocerSearchContainerPageIterator_itemsPerPage");
-    //By tableContainer = By.cssSelector(".taglib-search-iterator tr");
+    By selectItemsPerPage = By.cssSelector("#_evozonlibrary_WAR_EvozonLibraryportlet_ocerSearchContainerPageIterator_itemsPerPage option");
+    By tableContainer = By.cssSelector(".results-row:not(.lfr-template)");
 
 
     public void getHomepage() {
         driver.get("http://evoportal.evozon.com");
     }
 
-    public void clickOnSearchLink(){
+    public void clickOnSearchLink() {
         driver.findElement(searchLink).click();
     }
 
-    public void searchForSomething(String text){
+    public void searchForSomething(String text) {
         waitForElement(searchInput, 5000);
         driver.findElement(searchInput).sendKeys(text);
         driver.findElement(searchInput).sendKeys(ENTER);
     }
 
-    public void selectRandomElementFromList(){
+    public void selectRandomElementFromList() {
         waitForElement(searchResults, 5000);
         List<WebElement> resultsList = driver.findElements(searchResults);
         //System.out.println("Lista are " + resultsList.size() + " elemente");
-        WebElement element = resultsList.get(randomGenerator(0, (resultsList.size()-1)));
+        WebElement element = resultsList.get(randomGenerator(0, (resultsList.size() - 1)));
         String text = element.getText();
         element.click();
         waitForElement(bodyContent, 5000);
@@ -48,17 +49,22 @@ public class DanaPage extends BasePage{
         Assert.assertTrue(bodyOfPage.getText().contains(text));
     }
 
-    public void clickOnLibraryLink(){
+    public void clickOnLibraryLink() {
         driver.findElement(libraryLink).click();
     }
 
-    public void selectItemsNumberPerPage(){
+    public void selectItemsNumberPerPage() {
         waitForElement(selectItemsPerPage, 5000);
-        Select dropdown = new Select(driver.findElement(selectItemsPerPage));
-        dropdown.selectByValue("10");
-        //waitForElement(tableContainer, 5000);
-        //List<WebElement> tableRows = driver.findElements(tableContainer);
-
+        List<WebElement> dropdownValues = driver.findElements(selectItemsPerPage);
+        WebElement valueFromDropdown = dropdownValues.get(randomGenerator(0, (dropdownValues.size() - 1)));
+        valueFromDropdown.click();
+        String value = valueFromDropdown.getText();
+        //System.out.println("valoarea selectata: " + value);
+        waitForElement(tableContainer, 5000);
+        List<WebElement> tableRows = driver.findElements(tableContainer);
+        //System.out.println("lista are: " + tableRows.size());
+        String rowsNumber = tableRows.size() + "";
+        Assert.assertEquals(rowsNumber, value);
 
     }
 }
